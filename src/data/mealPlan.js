9dbd56dddc95c13fdf55473a3de005c7273daed7
divyareddykondakrindi <divyareddykondakrindi@gmail.com>
2026-03-01 22:43:11 +0530

    Update mealPlan.js with default meal options

---
+++ src/data/mealPlan.js
@@ -0,0 +1,8 @@
+const mealPlan = Array.from({ length: 30 }, () => ({
+  Breakfast: "Healthy Breakfast",
+  Lunch: "Balanced Lunch",
+  Snack: "Light Snack",
+  Dinner: "Light Dinner"
+}));
+
+export default mealPlan;
